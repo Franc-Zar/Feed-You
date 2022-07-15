608ecfbd709380002a5654aa9f2497c0c70fd8ac
Franc-Zar <s1093617@studenti.univpm.it>
2022-07-15 10:45:28 +0200

    rimozione cartella inutile

--- App/app/src/main/java/com/example/app1/NewsData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app1
-
-import tw.ktrssreader.kotlin.model.item.Guid
-
-data class NewsData (var title: String, var desc: String, var link:String,
-                     var category: String, var guid: Guid?){}
