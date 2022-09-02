e4478f942e9e3ef78e7843b00bade57b2cd8f475
Franc-Zar <s1093617@studenti.univpm.it>
2022-09-02 12:44:46 +0200

    mod

--- app/src/main/java/com/example/app1/settings/menu/AboutActivity.kt
+++ app/src/main/java/com/example/app1/settings/menu/AboutActivity.kt
@@ -1,9 +1,12 @@
 package com.example.app1.settings.menu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.app1.R
 import com.google.android.material.appbar.MaterialToolbar
+
 
 /** Activity riportante una breve descrizione dell'applicazione (about us)
  */
@@ -12,6 +15,8 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar_about_us)
+        val aboutUs = findViewById<TextView>(R.id.decoration_text)
+        aboutUs.movementMethod = ScrollingMovementMethod()
 
         toolbar.setNavigationOnClickListener {
 
